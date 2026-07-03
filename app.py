16a02d9c7e08febbc2c385c6b6cf6b114b7f56ca
Kishi <kcaushiv@gmail.com>
2026-07-02 23:07:44 -0500

    railway config

--- app.py
+++ app.py
@@ -251,4 +251,6 @@ def _seed_demo_data():
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    import os
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=False)
